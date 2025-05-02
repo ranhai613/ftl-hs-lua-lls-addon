4ce1c35da507c3a291e6e818be047dd2cd8d0acc
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2025-05-03 00:04:41 +0900

    clean up generating stuff

--- parseSource/update_parse.py
+++
@@ -1,7 +0,0 @@
-import parseHS_directory, parseWiki, parseWiki, parseWiki_Defines, parse
-
-if __name__ == "__main__":
-    parseHS_directory.main()
-    parseWiki.main()
-    parseWiki_Defines.main()
-    parse.main()
